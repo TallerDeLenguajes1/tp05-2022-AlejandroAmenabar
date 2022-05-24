5a7909ca527d78c0f30b349cef3b782129a76467
Alejandro Amenabar <alegymnas@gmail.com>
2022-05-23 23:15:30 -0300

    punto2 CV1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,23 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hola mundo!");
-Console.WriteLine("A ver si sale");
+/*Console.WriteLine("Ejercicio 1: invertir numero de N cifras");
+Console.WriteLine("ingrese el numero: ");
 
-Console.WriteLine("Ingrese un numero");
-string numero1 = Console.ReadLine();
-Console.WriteLine("Ingrese otro numero");
-string numero2 = Console.ReadLine();
-Console.WriteLine("Ingrese otro numero");
-string numero3 = Console.ReadLine();
-Console.WriteLine("Ingrese otro numero");
-string numero4 = Console.ReadLine();
+int num = Convert.ToInt32(Console.ReadLine());
+int numInvertido = 0;
+Console.WriteLine("El numero ingresado es:  "+num);
 
-double cuadrado = Cuadrado(Convert.ToDouble(numero2));
-double resultado = Convert.ToDouble(numero1) + cuadrado + Convert.ToDouble(numero3);
-
-Console.WriteLine("La suma de los dos numeros es "+ resultado);
-
-double Cuadrado(double a)
+while (num>0)
 {
-    return System.Math.Pow(a,2);
+    numInvertido = numInvertido *10 + (num%10);
+    num = num/10;
 }
+
+Console.WriteLine("El numero ingresado invertido es:  "+numInvertido
+);*/
+
+/*---Calculadora---*/
+
+int opcion=10;
+int menu=2;
+double num1,num2,resultado=0;
+do
+{
+    Console.WriteLine("elija la operacion a realizar");
+    Console.WriteLine("1-Sumar 2-Restar 3-Multiplicar 4-Dividir");
+    opcion = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese el primer numero: ");
+    num1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Ingrese el segundo numero: ");
+    num2 = Convert.ToDouble(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            resultado = num1 + num2;
+            break;
+        case 2:
+            resultado = num1 - num2;
+            break;
+        case 3:
+            resultado = num1 * num2;
+            break;
+        case 4:
+            if(num2!=0){
+                resultado = num1/num2;
+            }else{
+                Console.WriteLine("ERROR! Para dividir dos numeros el segundo debe ser distinto de cero");
+            }
+            break;
+        default:
+            Console.WriteLine("debe ingresar una opcion valida");
+            break;
+    }
+    Console.WriteLine("El resultado es: " + resultado);
+    Console.WriteLine("Desea realizar otra operacion 1-Si 0-No");
+    menu = Convert.ToInt32(Console.ReadLine());
+} while (menu!=0);
