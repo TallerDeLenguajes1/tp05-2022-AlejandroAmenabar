15709d24063e63d7aa77775a3556b677d4d28fb2
Alejandro Amenabar <alegymnas@gmail.com>
2022-05-23 23:50:07 -0300

    CalculadoraV2 finalizada

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -19,3 +19,25 @@ Console.WriteLine("El numero ingresado invertido es:  "+numInvertido);
 
 /*---Calculadora V2---*/
 
+using System;
+
+double num1, num2;
+
+Console.WriteLine("Ingrese un número");
+num1 =  Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Valor Absoluto: " + Math.Abs(num1));
+Console.WriteLine("Valor Cuadrado: " + Math.Pow(num1, 2));
+Console.WriteLine("Raíz Cuadrada: " + Math.Pow(num1, 0.5f));
+Console.WriteLine("Seno: " + Math.Sin(num1));
+Console.WriteLine("Coseno: " + Math.Cos(num1));
+Console.WriteLine("Parte Entera " + Math.Floor(num1));
+
+Console.WriteLine("Ingrese el primer número");
+num1 =  Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número");
+num2 =  Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("El máximo entre los dos es: " + Math.Max(num1, num2));
+Console.WriteLine("El mínimo entre los dos es: " + Math.Min(num1, num2));
